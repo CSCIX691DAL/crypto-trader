a00f419836e5aa79d62d85f5280258d8ec5b72c0
Liam So <Liam.So@dal.ca>
2021-05-25 15:05:53 -0300

    Added component folder

---
+++ components/DashboardTable/DashboardTable.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const DashboardTable = () => {
+    return (
+        <div>
+            This is the Dashboard Table
+        </div>
+    )
+}
+
+export default DashboardTable
---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className="flex font-semibold text-3xl text-gray-800 justify-center">
+            This is the header
+        </div>
+    )
+}
+
+export default Header
