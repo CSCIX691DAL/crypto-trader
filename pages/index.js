9fdfe7b1ab0d9e2988fe7330c8bcd6e3e7fe43b0
Liam So <Liam.So@dal.ca>
2021-05-28 16:03:55 -0300

    Created REST API and sample HTTP service calls

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import Head from 'next/head'
 import Header from '../components/Header'
+import * as React from 'react'
+import { getUser } from '../services/user'
 
 export default function Home() {
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+
+    const getUserInfo = async() => {
+      // the userId is hard coded for now...
+      const res = await getUser('Z4ve1jKNCTxdTUNfeAHR');
+      setUser(res);
+    }
+
+    getUserInfo();
+    console.log(user)
+  }, []);
+
   return (
     <div>
       <Head>
@@ -12,7 +28,7 @@ export default function Home() {
 
       <Header/>
       <main className="m-8 flex font-semibold text-lg text-gray-800 justify-center">
-            Welcome to the Dashboard 🚀
+            Welcome to the Dashboard, {user ? user.name : ''} 🚀
       </main>
 
     </div>
---
+++ services/user.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+const USER_URL = `http://localhost:3000/api/user`;
+
+export const getUser = async (userID) => {
+    const res = await axios.get(`${USER_URL}/${userID}`);
+    return res.data;
+}
