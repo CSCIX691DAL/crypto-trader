9c5a6c6f232ab52710a4336c61a197f25e30218a
Joel Richards <joelrichards@dal.ca>
2021-06-04 15:20:09 -0300

    US-1: changes based on review feedback

--- pages/api/user/[id].js
+++ pages/api/user/[id].js
@@ -7,7 +7,8 @@ export default async (req, res) => {
         if (req.method === 'GET') {
             const user = (await db.collection('users').doc(id).get()).data();
             res.status(200).json(user);
-        } else if (req.method === 'POST') {
+        } 
+        else if (req.method === 'POST') {
             await db.collection('users').doc(id).set({
                 name: req.body.name,
                 email: req.body.email
@@ -18,7 +19,8 @@ export default async (req, res) => {
             }).catch((error) => {
                 console.error("Error adding user to database", error);
             });
-        } else if (req.method === 'DELETE') {
+        } 
+        else if (req.method === 'DELETE') {
             await db.collection('users').doc(id).delete();
         }
 
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,11 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import * as React from 'react'
 import { getUser, createUser } from '../services/user'
+import { getDashboardInfo } from '../services/coins'
 import MD5 from 'crypto-js/md5'
 import encoder from 'crypto-js/enc-hex'
 import { useSession } from 'next-auth/client'
-import axios from 'axios'
-import {useState,useEffect} from 'react'
-import DashboardTable from '../components/DashboardTable/DashboardTable.js'
+import DashboardTableItem from '../components/DashboardTable/DashboardTableItem.js'
 
 
 export default function Home() {
@@ -18,8 +17,8 @@ export default function Home() {
     const startSession = async(hash, email, name) => {
         const checkIfUserExists = await getUser(hash);
         if (!checkIfUserExists.data) {
-        const create = await createUser(hash, email, name);
-        console.log("Created user", create);
+            const create = await createUser(hash, email, name);
+            console.log("Created user", create);
         }
     }
 
@@ -28,33 +27,31 @@ export default function Home() {
         startSession(hash, session.user.email, session.user.name);
     }
 
-    
-    
+    // get user info from Firebase
     React.useEffect(() => {
-
         const getUserInfo = async(email) => {
-        let hash = MD5(email).toString(encoder);
-        const res = await getUser(hash);
-        setUser(res.data);
+            let hash = MD5(email).toString(encoder);
+            const res = await getUser(hash);
+            setUser(res.data);
         }
 
         getUserInfo(session ? MD5(session.user.email).toString(encoder) : '');
     }, []);
     
 
-    const [coins, setCoins] = useState([]);
-
+    // grab coin details/pics from CoinGecko
+    const [coins, setCoins] = React.useState([]);
     React.useEffect(() => {
-        axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
-        .then(res =>{
-            setCoins(res.data);
-        }).catch(error => console.log(error))
+        const getCoinInfo = async() => {
+            const data = await getDashboardInfo();
+            setCoins(data);
+        }
+
+        getCoinInfo();
     }, []);
 
-    const coindisp = coins.filter(coin => 
-        coin);
+    const coindisp = coins.map(coin => coin);
         
-
     return (
         <div>
         <Head>
@@ -73,17 +70,7 @@ export default function Home() {
         <div className='m-8 flex font-normal text-base text-gray-800 '>
             <h1 className='coin-text'>Coins Table</h1>
             {coindisp.map(coin => {
-                return <DashboardTable
-                    key ={coin.id}
-                    name ={coin.name} 
-                    image = {coin.image}
-                    symbol = {coin.symbol}
-                    priceChange ={coin.price_change_percentage_24h}
-                    price ={coin.current_price}
-
-                >
-                      </DashboardTable>
-                
+                return <DashboardTableItem coin={coin} />  
             })}
         </div>
 
