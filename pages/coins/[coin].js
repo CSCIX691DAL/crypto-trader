68d6b8f77767afa0f9fb56687726e0bcc55d29be
Joel Richards <joelrichards@dal.ca>
2021-06-07 16:47:29 -0300

    US-9: add purchasing functionality, no interation with Firebase yet

--- pages/coins/[coin].js
+++ pages/coins/[coin].js
@@ -3,15 +3,25 @@ import Header from '../../components/Header'
 import * as React from 'react'
 import { useSession } from 'next-auth/client'
 import { useRouter } from 'next/router'
+import MD5 from 'crypto-js/md5'
+import encoder from 'crypto-js/enc-hex'
 
 export default function Coin() {
     const [ session, loading ] = useSession();
     const router = useRouter();
+    const [ amount, setAmount ] = React.useState('');
 
     const { coin } = router.query;
 
+    function purchase() {
+        const hash =  MD5(session.user.email).toString(encoder);
+        console.log("hash:", hash);
+        console.log("Purchasing", amount, "of",  {coin}.coin, "for user", session.user.email);
+        return false;
+    }
+
     return (
-        <div>
+        <>
             <Head>
                 <title>CryptoTrader</title>
                 <meta name="description" content="View prices and trade cryptocurrencies" />
@@ -19,11 +29,24 @@ export default function Coin() {
             </Head>
 
             <Header/>
-            <main className="m-8 flex font-semibold text-lg text-gray-800 justify-center">
-                <p>Here's some info about {coin} 💰</p>
-            </main>
-
-        </div>
-    
+            <div className="m-8 flex flex-col font-semibold text-lg text-gray-800 justify-center">
+                <div className="px-4 py-2 m-2">
+                    Here's some info about {coin} 💰
+                </div>
+                
+                <div className="items-center m-2">
+                    <form>
+                        <input type="number" min="0" placeholder="Amount" onChange={event => setAmount(event.target.value)} className="mr-2 border-b border-blue-500" />
+                        {
+                            session ? (
+                                <button onClick={() => purchase()} className="rounded-xl p-2 bg-blue-500 text-white" type="button">Purchase</button>
+                            ) : (
+                                <button className="rounded-xl p-2 bg-gray-200 text-white" type="button">Purchase</button>
+                            )
+                        }
+                    </form>
+                </div> 
+            </div>
+        </>
     )
 }
