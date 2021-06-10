d865c98556676b570e567a387af62b8812b7c786
Liam So <Liam.So@dal.ca>
2021-06-10 15:06:55 -0300

    Fixed formatting

--- components/DashboardTable/DashboardTableItem.js
+++ components/DashboardTable/DashboardTableItem.js
@@ -6,22 +6,22 @@ const DashboardTableItem = ({ coin }) => {
         <div className='coin-container'>
             <div className='coin-row'>
                 <div className='coin'>
-                    <img src={coin.image} alt="crypto"/>
-                
-                <p className='coin-symbol'>{coin.symbol} </p>
-                
-                <Link key={coin.id} href={`/coins/${coin.id}`}>
+                    <img src={coin.image} alt="crypto" />
+
+                    <p className='coin-symbol'>{coin.symbol} </p>
+
+                    <Link key={coin.id} href={`/coins/${coin.id}`}>
                         <h1 className="cursor-pointer text-xl font-sans text-gray-700 font-semibold">{coin.id}</h1>
                     </Link>
                 </div>
-            <div className='coin-data'>
-                <p className='coin-price'>${coin.current_price}</p>
+                <div className='coin-data'>
+                    <p className='coin-price'>${coin.current_price}</p>
 
-                <p className='coin-percent'>{coin.price_change_percentage_24h.toFixed(2)}%</p>
-                
+                    <p className='coin-percent'>{coin.price_change_percentage_24h.toFixed(2)}%</p>
+
+                </div>
             </div>
-            </div>
-            
+
         </div>
     )
 }
