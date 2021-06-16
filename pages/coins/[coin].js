3112abe21cc487842493ae2751daa0e7c87c1866
gufran <gf316128@dal.ca>
2021-06-16 08:57:24 +0400

    logging data onto chart

--- pages/coins/[coin].js
+++ pages/coins/[coin].js
@@ -7,15 +7,70 @@ import MD5 from 'crypto-js/md5'
 import encoder from 'crypto-js/enc-hex'
 import { purchase } from '../../services/transactions'
 
+import { useEffect } from 'react'
+import axios from 'axios'
+import {COIN_GECKO_URL} from '../../services/coins'
+import PriceChart from '../../components/PriceChart'
+
+
+
+
+
 export default function Coin() {
     const [ session, loading ] = useSession();
     const router = useRouter();
     const [ amount, setAmount ] = React.useState('amount');
     const { coin } = router.query;
+    const coinName = {coin}.coin;
+    const COIN_GECKO_URL = `https://api.coingecko.com/api/v3/`;
 
     const hashEmail = () => {
         return MD5(session.user.email).toString(encoder);
     }
+    const [coinData, setCoinData] = React.useState({})
+    const format = data => {
+        return data.map(el => {
+            return {
+                t: el[0],
+                y: el[1].toFixed(2)
+            }
+        })
+    }
+
+         useEffect(() => {
+         const fetchdata = async() =>{
+            const resDay = await axios.get(`${COIN_GECKO_URL}coins/${coinName}/market_chart`, {
+             params: {
+                 vs_currency: "usd",
+                days: "1",
+             },
+            });
+            const resWeek = await axios.get(`${COIN_GECKO_URL}coins/${coinName}/market_chart`, {
+                params: {
+                    vs_currency: "usd",
+                   days: "7",
+                },
+               });
+               const resMonth = await axios.get(`${COIN_GECKO_URL}coins/${coinName}/market_chart`, {
+                params: {
+                    vs_currency: "usd",
+                   days: "800",
+                },
+               });
+            
+            setCoinData({
+                day: format(resDay.data.prices),
+                week: format(resWeek.data.prices),
+                 month: format(resMonth.data.prices)
+                });
+               
+             
+
+        };
+    
+        coinName ? fetchdata(): ""; 
+        
+    }, [coinName])
 
     return (
         <>
@@ -30,6 +85,8 @@ export default function Coin() {
                 <div className="px-4 py-2 m-2">
                     Here's some info about {coin} 💰
                 </div>
+                <div><PriceChart data ={coinData}/></div>
+            
                 
                 <div className="items-center m-2">
                     <form>
