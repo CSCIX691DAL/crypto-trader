5619c87df1b6aa0b5058eca21857d68e617f73d6
Joel Richards <joelrichards@dal.ca>
2021-06-18 14:35:15 -0300

    US-2: center items on details page

--- pages/coins/[coin].js
+++ pages/coins/[coin].js
@@ -101,7 +101,7 @@ export default function Coin() {
 
             <Header />
 
-            <div className="flex m-8 flex-col font-semibold text-lg text-gray-800 justify-center">
+            <div className="flex m-8 flex-col font-semibold text-lg text-gray-800 justify-center items-center">
                 <div className="px-4 py-2 m-2 text-2xl">
                     Crypto Currency {coin} 💰
                 </div>
