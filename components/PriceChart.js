df101f2248479eea3687ec45d3767464bdc92c06
Liam So <Liam.So@dal.ca>
2021-06-19 11:59:07 -0300

    Added table responsiveness and cleaned up code

--- components/PriceChart.js
+++ components/PriceChart.js
@@ -1,89 +1,80 @@
-import React, { useEffect, useRef, useState } from 'react'
+import * as React from 'react'
 import Chartjs from 'chart.js'
 import { historyOptions } from '../services/ChartConfig';
 
+const PriceChart = ({ data }) => {
+  const chartRef = React.useRef()
+  const { day, week, year } = data
+  const [time, setTimeFormat] = React.useState("24h");
 
-const PriceChart = ({data}) => {
-    const chartRef = useRef()
-    const{day,week,year} = data
-    const [time, setTimeFormat] = useState("24h");
-
-    const TimeFormat = () => {
-        
-
-    if(time === "24h"){
-        return day
+  const TimeFormat = () => {
+    if (time === "24h") {
+      return day
     }
-    else if(time === "7d"){
-        return week
+    else if (time === "7d") {
+      return week
     }
-    else if(time === "1Y"){
-        return year 
+    else if (time === "1Y") {
+      return year
     }
-       
-      };
+  };
 
-    useEffect(() => {
-        
-        if (window.MyChart != undefined)
-{
-    window.MyChart.destroy();
-}
- 
-        if(chartRef && chartRef.current){
-            window.MyChart = new Chartjs(chartRef.current, {
-                type: 'line', 
-                data: {
+  React.useEffect(() => {
+    if (window.MyChart != undefined) {
+      window.MyChart.destroy();
+    }
 
-                    datasets :[
-                        {
-                            label: "Price",
-                            data: TimeFormat(),
-                            backgroundColor: "rgba(44, 130, 201, 1)",
-                            borderWidth: 1,
-                      
-                            },
-                    ],
-                    
-                },
-                options: {
-                    ...historyOptions,
-                },
-            } );
-        }
+    if (chartRef && chartRef.current) {
+      window.MyChart = new Chartjs(chartRef.current, {
+        type: 'line',
+        data: {
 
-    })
-    return (
+          datasets: [
+            {
+              label: "Price",
+              data: TimeFormat(),
+              backgroundColor: "rgba(44, 130, 201, 1)",
+              borderWidth: 1,
 
-        <div className= "bg-white border mt-2 rounded p-3">
-            
-        <div></div>
-        <div>
-            <canvas ref={chartRef} id= "myChart" width = {250} height = {250}></canvas>
-        </div>
-        
-    
-        <button
-          onClick={() => setTimeFormat("24h")}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-4 rounded"
-        >
-          24h
-        </button>
-        <button
-          onClick={() => setTimeFormat("7d")}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-4 rounded"
-        >
-          7d
-        </button>
-        <button
-          onClick={() => setTimeFormat("1Y")}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-4 rounded"
-        >
-          1Y
-        </button>
+            },
+          ],
+
+        },
+        options: {
+          ...historyOptions,
+        },
+      });
+    }
+  });
+
+  return (
+
+    <div className="bg-white border mt-2 rounded p-3">
+      <div>
+        <canvas ref={chartRef} id="myChart" width={250} height={250}></canvas>
       </div>
-       
-    )
+
+      <button
+        onClick={() => setTimeFormat("24h")}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-4 rounded"
+      >
+        24h
+      </button>
+      <button
+        onClick={() => setTimeFormat("7d")}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-4 rounded"
+      >
+        7d
+      </button>
+      <button
+        onClick={() => setTimeFormat("1Y")}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-4 rounded"
+      >
+        1Y
+      </button>
+    </div>
+
+  )
 };
 
 export default PriceChart
