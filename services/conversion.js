421a1ab67a14abedb22838323c2a895d8d082e9b
Liam So <Liam.So@dal.ca>
2021-06-28 10:54:55 -0300

    US-3-AT-1: Removed redundant console logs

--- services/conversion.js
+++ services/conversion.js
@@ -1,11 +1,6 @@
 export const convertCrypto = (convertFrom, convertTo, amountOfFirstCurr) => {
-    console.log(convertFrom, convertTo)
-
     if (convertFrom && convertTo) {
         const ratio = convertFrom.current_price / convertTo.current_price;
-
-        console.log(ratio)
-
         const res = ratio * amountOfFirstCurr;
 
         return `${amountOfFirstCurr} ${convertFrom.name} = ${res} ${convertTo.name}`;
