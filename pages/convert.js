88791be390cf3998b87869d89cae4a23702060ac
Joel Richards <joelrichards@dal.ca>
2021-06-30 12:04:58 -0300

    US-3: fix colours of convert page

--- pages/convert.js
+++ pages/convert.js
@@ -35,11 +35,11 @@ export default function Convert() {
       <div className="flex m-8 flex-col font-semibold text-lg text-gray-800 justify-center items-center">
                               
         <div className=" bg-white shadow overflow-hidden sm:rounded justify-center items-center w-11/12 md:w-1/2" >
-          <div className="px-4 py-5 sm:px-6 bg-green-200">
+          <div className="px-4 py-5 sm:px-6 bg-blue-300">
             <h3 className="text-xl leading-6 font-medium text-gray-900  text-center "> Currency Convert</h3>
 
           </div>
-        <div className="border-t border-gray-200">
+        <div className="border-t border-gray-200 bg-gray-50">
           <dl>
 {/*-----------------Enter First Coin and dropdown and amount-----------------*/}
 
@@ -82,7 +82,7 @@ export default function Convert() {
               <div className=" justify-center items-center">
                 <div className="flex flex-row">
                 {dropdown[convertTo] && (
-                  <button className="px-3 py-2 rounded-full bg-blue-400 ">
+                  <button className="px-3 py-2 rounded-full bg-blue-300">
                   <Link href={`/coins/${dropdown[convertTo].id}`}><a>Click here to buy {dropdown[convertTo].id}</a></Link>
                   </button>
                   )}
@@ -91,16 +91,16 @@ export default function Convert() {
           </div>
 
  {/*---------------------Convert button & output values---------------------*/}
-              <div className="flex m-10 justify-center">
+              <div className="flex m-10 justify-center bg-gray-50">
                                
                 <button onClick={() => {
                   setConvertedValue(convertCrypto(dropdown[convertFrom], dropdown[convertTo], amountOfFirstCurr));
-                  }} className="px-10 py-2 rounded-full bg-green-200 shadow">Convert</button>
+                  }} className="px-10 py-2 rounded-full bg-blue-300 shadow">Convert</button>
 
               </div>
 
               <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-1 sm:gap-4 sm:px-6">
-                <dt className=" text-lg font-medium text-gray-500 text-center"><p>{convertedValue}</p></dt>
+                <dt className=" text-lg text-gray-800 font-semibold text-center"><p>{convertedValue}</p></dt>
                                 
               </div>
             </dl>
